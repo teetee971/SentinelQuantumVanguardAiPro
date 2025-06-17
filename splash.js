86456b28a83e497582bf5b4da932a7059cbee2cf
teetee971 <95961170+teetee971@users.noreply.github.com>
2025-06-17 03:48:44 -0400

    Add files via upload

---
+++ splash.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', () => {
+    const splash = document.getElementById('splash');
+    splash.style.opacity = 0;
+    setTimeout(() => splash.style.display = 'none', 2000);
+});
