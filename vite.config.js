520f0059b3c354efa34e1a06c36def6b492f009c
teetee971 <teetee971@users.noreply.github.com>
2025-08-11 16:11:07 -0400

    Fix config Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/ia': 'http://localhost:5001' // Pour tests locaux
-    }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_APP_ENV || 'production'),
+    },
+    server: {
+      proxy: {
+        '/api/ia': 'http://localhost:5001',
+      },
+    },
   }
 })
