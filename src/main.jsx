9d629b52678b343d1ee77d0cb8f860e1239a9530
teetee971 <95961170+teetee971@users.noreply.github.com>
2025-10-11 13:20:37 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import IAHealthMonitor from "./components/IAHealthMonitor";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import IADashboard from "./components/IADashboard";
+import IAHealthMonitor from "./components/IAHealthMonitor";
 
-function App() {
-  return (
-    <>
-      <IADashboard />
-      <IAHealthMonitor />
-    </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <IADashboard />
+    <IAHealthMonitor />
+  </React.StrictMode>
+);
