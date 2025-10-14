1eaf3f5651f127dd7039c89e9a0d459edbfe3afa
teetee971 <teetee971@github.com>
2025-10-14 16:23:43 -0400

    🌐 Add Wave 23 – Sentinel AI Network Console (WebSocket Live Monitor)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CommandCenter from "./admin/CommandCenter.jsx";
+import NetworkConsole from "./network/NetworkConsole.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CommandCenter />
+    <NetworkConsole />
   </React.StrictMode>
 );
