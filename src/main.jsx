f97d5b527655ccae38e854dfc993bd38e58543c6
teetee971 <teetee971@github.com>
2025-10-14 16:22:33 -0400

    🧠 Add Wave 22 – Sentinel Command Center (Admin IA Panel)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RealtimeMap from "./realtime/RealtimeMap.jsx";
+import CommandCenter from "./admin/CommandCenter.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RealtimeMap />
+    <CommandCenter />
   </React.StrictMode>
 );
