d0943172a3b53425bc2c476c8ca299b586f7c2a6
teetee971 <teetee971@github.com>
2025-10-14 16:19:56 -0400

    🌍 Add Wave 21 – Realtime Map & Global Health Monitor (Leaflet + React)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./dashboard/Dashboard.jsx";
+import RealtimeMap from "./realtime/RealtimeMap.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <RealtimeMap />
   </React.StrictMode>
 );
