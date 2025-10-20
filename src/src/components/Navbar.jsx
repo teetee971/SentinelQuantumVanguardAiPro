5b0899595d86ffd50b8728583c5b37e3aef2a0c8
teetee971 <95961170+teetee971@users.noreply.github.com>
2025-10-20 02:50:09 -0400

    Create src/components/Navbar.jsx

---
+++ src/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-center gap-6 py-6 text-cyan-300 text-lg">
+      <a href="#">Modules</a>
+      <a href="#">Actualités</a>
+      <a href="#">Blog</a>
+      <a href="#">Télécharger</a>
+    </nav>
+  )
+}
