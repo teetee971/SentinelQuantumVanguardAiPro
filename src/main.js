db14ce1f2fdd0ffeb782f5fb78fd6dd66d0fcdfa
teetee971 <95961170+teetee971@users.noreply.github.com>
2025-10-20 02:23:22 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-document.getElementById('app').textContent = 'Hello from Vite!';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
