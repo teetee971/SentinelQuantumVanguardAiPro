944086f354298f721442296569d4e7f84e39a75f
teetee971 <thierrynaud2009@gmail.com>
2026-09-18 08:40:45 -0400

    android: wire default SMS status into navigation

--- native-android-app/app/src/main/java/com/sentinel/quantum/navigation/NavGraph.kt
+++ native-android-app/app/src/main/java/com/sentinel/quantum/navigation/NavGraph.kt
@@ -47,6 +47,9 @@ fun NavGraph(
         composable(Screen.SmsScanner.route) {
             SmsScannerScreen(navController = navController)
         }
+        composable(Screen.DefaultSmsStatus.route) {
+            DefaultSmsStatusScreen(navController = navController)
+        }
         composable(Screen.AppPermissionAnalyzer.route) {
             AppPermissionAnalyzerScreen(navController = navController)
         }
