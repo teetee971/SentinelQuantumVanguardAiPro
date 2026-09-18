e50df3f1baa83e7830e8a2fd80ea1abdd9e7a601
teetee971 <thierrynaud2009@gmail.com>
2026-09-18 08:47:12 -0400

    android: rebase VPN UI (NavGraph.kt)

--- native-android-app/app/src/main/java/com/sentinel/quantum/navigation/NavGraph.kt
+++ native-android-app/app/src/main/java/com/sentinel/quantum/navigation/NavGraph.kt
@@ -56,6 +56,9 @@ fun NavGraph(
         composable(Screen.DigitalExposure.route) {
             DigitalExposureScreen(navController = navController)
         }
+        composable(Screen.DefensiveVpn.route) {
+            VpnSecurityScreen(navController = navController)
+        }
         composable(Screen.About.route) {
             AboutScreen(navController = navController)
         }
