d09ec43bfd94ed4f4f1949c8731c0350fe661cb4
teetee971 <thierrynaud2009@gmail.com>
2026-09-21 12:41:37 -0400

    Communications: test external messaging links

---
+++ native-android-app/app/src/test/java/com/sentinel/quantum/security/ExternalMessagingLinksTest.kt
@@ -0,0 +1,24 @@
+package com.sentinel.quantum.security
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class ExternalMessagingLinksTest {
+    @Test fun buildsSupportedPublicHandles() {
+        assertEquals("https://t.me/sentinel_user", ExternalMessagingLinks.url(ExternalMessagingLinks.Service.TELEGRAM, "@sentinel_user"))
+        assertEquals("https://www.instagram.com/sentinel.user/", ExternalMessagingLinks.url(ExternalMessagingLinks.Service.INSTAGRAM, "sentinel.user"))
+        assertEquals("https://m.me/sentinel_user", ExternalMessagingLinks.url(ExternalMessagingLinks.Service.MESSENGER, "sentinel_user"))
+    }
+
+    @Test fun rejectsUriInjectionAndPathTraversal() {
+        assertNull(ExternalMessagingLinks.url(ExternalMessagingLinks.Service.TELEGRAM, "user?text=secret"))
+        assertNull(ExternalMessagingLinks.url(ExternalMessagingLinks.Service.INSTAGRAM, "../user"))
+        assertNull(ExternalMessagingLinks.url(ExternalMessagingLinks.Service.MESSENGER, "user/name"))
+    }
+
+    @Test fun doesNotInventUnsupportedDestinations() {
+        assertNull(ExternalMessagingLinks.url(ExternalMessagingLinks.Service.SIGNAL, "sentinel"))
+        assertNull(ExternalMessagingLinks.url(ExternalMessagingLinks.Service.DISCORD, "sentinel"))
+    }
+}
