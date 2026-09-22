33a0184d1e1d3650a86236e5d55bb70f5b45819e
teetee971 <thierrynaud2009@gmail.com>
2026-09-22 06:48:12 -0400

    Phone Core: record implemented multi-SIM SMS support

--- native-android-app/app/src/main/java/com/sentinel/quantum/security/SmsRoleMigrationPolicy.kt
+++ native-android-app/app/src/main/java/com/sentinel/quantum/security/SmsRoleMigrationPolicy.kt
@@ -47,6 +47,7 @@ object SmsRoleMigrationPolicy {
         SmsClientCapability.SEND_SMS,
         SmsClientCapability.NOTIFICATIONS,
         SmsClientCapability.EMERGENCY_MESSAGES,
+        SmsClientCapability.MULTI_SIM,
         SmsClientCapability.LOCAL_RETENTION,
         SmsClientCapability.EXPORT_AND_DELETE,
         SmsClientCapability.OFFLINE_ANALYSIS
